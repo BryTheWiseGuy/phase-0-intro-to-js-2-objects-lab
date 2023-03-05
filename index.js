8a1db87a2ec0032365e72df24372f3c8fbc74049
Brian Ashman <brian.ashman@comcast.net>
2023-03-04 19:53:35 -0700

    Complete Objects Lab

--- index.js
+++ index.js
@@ -15,14 +15,13 @@ function destructivelyUpdateEmployeeWithKeyAndValue (employee, name, value) {
     return employee;
 }
 
-function deleteFromEmployeeByKey (employee, name, value) {
+function deleteFromEmployeeByKey (employee, name) {
     const newEmployee = {employee};
-    newEmployee[name] = value;
-    delete newEmployee.name;
+    delete newEmployee[name];
     return newEmployee;
 }
 
-function destructivelyDeleteFromEmployeeByKey (employee) {
-    delete employee.name;
+function destructivelyDeleteFromEmployeeByKey (employee, name) {
+    delete employee[name];
     return employee;
 }
